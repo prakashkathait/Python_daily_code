a8b94dd48c9194cf3b398c2674648c3f26d2ad4c
prakashkathait <prakash24kathait@gmail.com>
2024-09-29 23:42:14 +0530

    Day31 files added

---
+++ Day-31/calendar_module.py
@@ -0,0 +1,9 @@
+import calendar
+month,day,year = list(map(int,input().split()))
+def guess_day(month,day,year):
+    weekday = calendar.weekday(year,month,day)
+    weekday_cap = calendar.day_name[weekday].upper()
+    return weekday_cap
+
+result = guess_day(month,day,year)
+print(result)
